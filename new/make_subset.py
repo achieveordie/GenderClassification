200508663d1bf177c59a78073f7c324cf7cfde88
Sagar Mishra <54197164+achieveordie@users.noreply.github.com>
2021-04-07 13:36:16 +0530

    add sample data and py version of prediction for old sav

---
+++ new/make_subset.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+np.set_printoptions(precision=3)
+
+csv_location = str(Path(__file__).resolve().parents[2] / r'data/gender_final.csv')
+small_csv_location = (r"gender_final_small.csv",  # save here for remote reference and one in data directory
+                      str(Path(__file__).resolve().parents[2] / r'data/gender_final_small.csv'))
+new_data_size = 1000  # only have 1000 names, 50% of each type.
+
+complete_df = pd.read_csv(csv_location)
+complete_df.drop(columns=['Score'], inplace=True)
+males_df = complete_df[complete_df.Gender == 1]
+females_df = complete_df[complete_df.Gender == 0]
+num_males = males_df.Gender.count()
+num_females = females_df.Gender.count()
+
+assert num_males > new_data_size/2 and num_females > new_data_size/2
+
+new_df = pd.concat([
+    males_df.sample(n=int(new_data_size/2)),
+    females_df.sample(n=int(new_data_size/2))
+], axis=0, ignore_index=True)
+
+new_df = new_df.sample(frac=1).reset_index(drop=True)  # reset index and drop original index
+# new_df.to_csv(small_csv_location[0], index=False)
+# new_df.to_csv(small_csv_location[1], index=False)
+
+[new_df.to_csv(i, index=False) for i in small_csv_location]
---
+++ new/old_predictor.py
@@ -0,0 +1,52 @@
+"""
+Predictor code for old-GBM based classification, uses sources present in `old` directory.
+One can directly pass name(s) in the command line or wait for the program to prompt.
+"""
+
+import pickle
+import argparse
+import sys
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import MaxAbsScaler
+from timeit import default_timer as timer
+
+
+def predict(X):
+    print("Received: ", X)
+    if isinstance(X, tuple):
+        X = vectorizer.fit_transform(X)
+    else:
+        X = vectorizer.fit_transform([X])
+    X = scaler.fit_transform(X)
+    predictions = loaded_model.predict_proba(X)
+    if isinstance(predictions, list):
+        [print(i) for i in predictions]
+    else:
+        print(predictions)
+
+
+if __name__ == '__main__':
+    start = timer()
+    parser = argparse.ArgumentParser(description="The central code to predict the gender(s) based on name(s)")
+    parser.add_argument('names', metavar='-n', nargs='*', help='Enter name(s) if not then input when asked.')
+
+    dict_saved_location = r'../old/feature.pkl'
+    model_location = r'../old/genderClassificationV4.sav'
+    loaded_model = pickle.load(open(model_location, 'rb'))
+
+    vectorizer = CountVectorizer(analyzer='char_wb', ngram_range=(1, 7),
+                                 vocabulary=pickle.load(open(dict_saved_location, 'rb')))
+    scaler = MaxAbsScaler()
+    args = parser.parse_args()
+    if len(sys.argv) > 2:
+        names = tuple(args.names)
+    elif len(sys.argv) == 2:
+        names = str(args.names[0])
+    else:
+        names = input("No command-line argument found for names, enter single name or space separated list of names\n")
+        names = names.split(' ')
+        if len(names) == 1:  # single element list should be converted back to a string
+            names = names[0]
+
+    predict(names)
+    print("Total Time->", timer() - start)
